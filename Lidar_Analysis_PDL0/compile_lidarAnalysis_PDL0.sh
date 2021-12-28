994e0116be8ae34a02a2dd8705a6ce34f5bce89e
Juan Vicente Pallotta <juanpallotta@gmail.com>
2021-12-28 14:39:52 -0300

    Delete some unnecesary files

--- Lidar_Analysis_PDL0/compile_lidarAnalysis_PDL0.sh
+++ Lidar_Analysis_PDL0/compile_lidarAnalysis_PDL0.sh
@@ -8,12 +8,4 @@ echo "Building lidarAnalysis_PDL0..."
 
 make -f makefile_lidarAnalysis_PDL0
 
-# LIDAR_MAIN_FILE='mergeLidarFiles.cpp' 
-# LIDAR_LIBS_FILES='../libLidar/lidarDataTypes.hpp ../libLidar/libLidar.hpp ../libLidar/libLidar.cpp ../libLidar/lidarMathFunc.hpp ../libLidar/lidarMathFunc.cpp'
-# NETCDF_LIB_LIDAR='../libLidar/CNetCDF_LIDAR.hpp ../libLidar/CNetCDF_LIDAR.cpp'
-# NETCDF_LIB='-L/usr/local/lib -lnetcdf -I/usr/local/include'
-# LIDAR_OUT='mergeLidarFiles'
-
-# g++ ${LIDAR_MAIN_FILE} ${LIDAR_LIBS_FILES} ${NETCDF_LIB_LIDAR} ${NETCDF_LIB} -Wall -o ${LIDAR_OUT}
-
 exit 0
