eaf524c93e1df7d9498c7feb4c360e6f496fd899
Juan Pallotta <juanpallotta@gmail.com>
2022-02-17 09:59:16 -0300

    updated

--- Lidar_Configuration_Files/LPP_Settings.sh
+++ Lidar_Configuration_Files/LPP_Settings.sh
@@ -9,16 +9,17 @@ L2="yes"
 #* RELATIVE INPUT PATH TO THE TEST LIDAR FILES FOLDER INCLUDED IN THE REPOSITORY
 #* DO NOT USE SPACES!!!! 
 # PATH_IN="../signalsTest/Brazil/Manaus/"
-PATH_IN="/home/juan/MEGAsync/procesamiento/LidarAnalysisCode/LPP/signalsTest/Brazil/Manaus/2011/"
+# PATH_IN="/home/juan/MEGAsync/procesamiento/LidarAnalysisCode/LPP/signalsTest/Brazil/Manaus/2011/"
+PATH_IN="C:\Users\jpall\Dropbox\procesamiento\LidarAnalysisCode\LPP\signalsTest\Brazil\Manaus\2011"
 # PATH_IN="../signalsTest/Brazil/SPU/20210730/"
 # PATH_IN="/mnt/Disk-1_8TB/Argentina/Aeroparque/volcan_Toga_AER/"
 
 # CONFIGURATION FILES --> RELATIVE TO THE FOLDER WICH IS RUNNING THE ANALYSIS!!
 # BRAZIL
-FILE_CONF_L0="/home/juan/MEGAsync/procesamiento/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_PDL0_Brazil.conf"
-FILE_CONF_L1_L2="/home/juan/MEGAsync/procesamiento/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_PDL1_2_Brazil.conf"
-# FILE_CONF_L0="../Lidar_Configuration_Files/analysisParameters_PDL0_Brazil.conf"
-# FILE_CONF_L1_L2="../Lidar_Configuration_Files/analysisParameters_PDL1_2_Brazil.conf"
+# FILE_CONF_L0="/home/juan/MEGAsync/procesamiento/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_PDL0_Brazil.conf"
+# FILE_CONF_L1_L2="/home/juan/MEGAsync/procesamiento/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_PDL1_2_Brazil.conf"
+FILE_CONF_L0="C:\Users\jpall\Dropbox\procesamiento\LidarAnalysisCode\LPP\Lidar_Configuration_Files\analysisParameters_PDL0_Brazil.conf"
+FILE_CONF_L1_L2="C:\Users\jpall\Dropbox\procesamiento\LidarAnalysisCode\LPP\Lidar_Configuration_Files\analysisParameters_PDL1_2_Brazil.conf"
 
 # ARGENTINA
 # FILE_CONF_L0="../Lidar_Configuration_Files/analysisParameters_PDL0_Argentina.conf"
