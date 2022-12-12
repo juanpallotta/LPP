b75c3e10d0d2de262ef90ebeaabd446a34b782e4
Juan Vicente Pallotta <juanpallotta@gmail.com>
2022-12-12 18:58:16 -0300

    updated

--- Lidar_Configuration_Files/LPP_Settings.sh
+++ Lidar_Configuration_Files/LPP_Settings.sh
@@ -11,19 +11,19 @@ L2="yes"
 # PATH_IN="/mnt/Disk-1_8TB/Argentina/multiangulo/20151105/ramVert/"
 # PATH_IN="/mnt/Disk-1_8TB/Argentina/Aeroparque/20221102/LPP_OUT/20221102_L0.nc"
 # PATH_IN="/mnt/Disk-1_8TB/Argentina/Bariloche/20150504/"
-# PATH_IN="/mnt/Disk-1_8TB/Argentina/Hector/20221130/data/sin_amp/"
+PATH_IN="/mnt/Disk-1_8TB/Argentina/Hector/20221206_sinAmp/data/LPP_OUT/data_L0_L1.nc"
 # PATH_IN="/mnt/Disk-1_8TB/Brazil/Manaus/2011/9/21/"
 
 # PATH_IN="/mnt/Disk-1_8TB/Brazil/SPU/20210730/LPP_OUT/20210730_L0.nc"
 # PATH_IN="/mnt/Disk-1_8TB/Brazil/SPU/20200914/lidar_signals/"
-PATH_IN="/mnt/Disk-1_8TB/Brazil/SPU/20200914/lidar_signals/LPP_OUT/lidar_signals_L0_L1.nc"
+# PATH_IN="/mnt/Disk-1_8TB/Brazil/SPU/20200914/lidar_signals/LPP_OUT/lidar_signals_L0_L1.nc"
 # PATH_IN="/mnt/Disk-1_8TB/Brazil/SPU/20200914/lidar_signals/LPP_OUT/lidar_signals_L0_L1.nc"
 
 # CONFIGURATION FILES
 # ! HOLGER SIMULATION
 # FILE_CONF="/home/juan/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_Holger.conf"
 # ! BRAZIL
-FILE_CONF="/home/juan/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_SPU.conf"
+# FILE_CONF="/home/juan/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_SPU.conf"
 # FILE_CONF="/home/juan/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_Manaus.conf"
 # ! ARGENTINA
 # BARILOCHE
@@ -32,5 +32,6 @@ FILE_CONF="/home/juan/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisPa
 # PILAR (CORDOBA), 2009-AGOSTO/SEPTIEMBRE
 # FILE_CONF="/home/juan/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_Pilar_2009_AgoSep.conf"
 # ! HECTOR
-# FILE_CONF="/home/juan/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_Hector.conf"
+FILE_CONF="/home/juan/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_Hector.conf"
+#! MULTIANGLE
 # FILE_CONF="/home/juan/LidarAnalysisCode/LPP/Lidar_Configuration_Files/analysisParameters_multRam.conf"
--- run_LPP_Analysis.sh
+++ run_LPP_Analysis.sh
@@ -81,6 +81,8 @@ PROCES_SENTRY=1
 
             PATH_FILE_IN_L1=$PATH_FILE_OUT_L0
             PATH_FILE_OUT_L1=${PATH_FILE_IN_L1%.*}"_L1.nc"
+            PATH_FILE_OUT_L1_CLOUD_DATA=${PATH_FILE_OUT_L1%.*}"_CLOUD_DATA.dat"
+            echo -e "\n-----> " PATH_FILE_OUT_L1_CLOUD_DATA: $PATH_FILE_OUT_L1_CLOUD_DATA
 
             PATH_FILE_IN_L2=$PATH_FILE_OUT_L1
             PATH_FILE_OUT_L2=${PATH_FILE_IN_L2%.*}"_L2.nc"
